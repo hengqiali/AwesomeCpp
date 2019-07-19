392848d03220fe67bc49630f8edd996a3a273e93
chali <43441423+nwpuhq@users.noreply.github.com>
2019-07-19 17:04:03 +0800

    讲解了 C++中的继承

---
+++ main4.cpp
@@ -0,0 +1,61 @@
+
+#include <iostream>
+
+using namespace std;
+
+/****************************************
+
+        C++ 构造函数和析构函数:
+
+        1. 构造函数在类创建对象时自动调用
+        2. 析构函数在类对象销毁时自动调用
+
+*****************************************/
+
+class Fruit{
+    public:
+        Fruit(){ //构造函数，创建对象时自动调用
+            cout << "创建Fruit" << endl;
+        }
+        ~Fruit(){ //前面带~，和名字和类名一样，代表是析构函数
+            cout << "销毁Fruit" << endl;
+        }
+        string getName();//先声明再实现
+        void setName(string n);//先声明再实现
+    private:
+        string name;  //名字
+        double weight;//重量
+        string color; //颜色
+        int number;   //个数
+};//注意分号
+
+string Fruit::getName(){  //::代表 作用域符，Fruit::getName 代表在Fruit类中的getName函数
+    return name;
+}
+
+void Fruit::setName(string n){
+    name = n;
+}
+
+/** Banana继承自Fruit **/
+class Banana:public Fruit{ // :public Fruit 代表继承Fruit
+    public:
+        Banana(){cout << "创建Banana" << endl;};
+        ~Banana(){cout << "销毁Banana" << endl;};
+};
+
+int main()
+{
+    string name;
+
+    Fruit fru; //创建类对象fru,此时会自动调用构造函数
+    Banana ban;
+    cout << "子类会同时继承父类的构造函数和析构函数，以及public方法和变量" << endl;
+    cout << "Banana类虽然没有实现setName()和getName()，但是它继承了Fruit父类的这两个函数" << endl;
+    ban.setName("banana");  //类对象fru调用函数
+    name = ban.getName();
+    cout << "Banana's name = " << name << endl;
+
+    return 0;
+} //在作用域结束后,fru对象会销毁掉，此时会自动调用构造函数
+
