48e740a5bf5d516115da70ede52be14c7ccefbca
chali <43441423+nwpuhq@users.noreply.github.com>
2019-07-22 17:07:35 +0800

    讲解了异常处理

---
+++ main15.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+
+using namespace std;
+
+/****************************************
+
+            C++ 异常
+
+*****************************************/
+
+int main()
+{
+    int x;
+    int y;
+
+    cout << "请输入整数：" << endl;
+    cout << "x = ";
+    cin >> x;
+    cout << "y = ";
+    cin >> y;
+
+    try   //try包含需要异常检查的代码块
+    {
+        if(x < y)
+            throw 0;  //抛出异常1
+        else
+            throw 1;  //抛出异常2
+    }
+    catch(int e)  //捕获异常
+    {
+        switch(e) //根据异常类型分别处理
+        {
+        case 0:
+            cout << "x 小于 y" << endl;
+            break;
+        case 1:
+            cout << "x 大于 y" << endl;
+            break;
+        }
+    }
+}
