4150194382b1707ae8bdfe61227618a9dc7389c4
chali <43441423+nwpuhq@users.noreply.github.com>
2019-08-24 21:45:08 +0800

    alloc、malloc、calloc和realloc

---
+++ main33.cpp
@@ -0,0 +1,8 @@
+https://blog.csdn.net/liuhuiyi/article/details/8216663 
+https://blog.csdn.net/weizhengbo/article/details/54381958
+http://www.voidcn.com/article/p-glcmjmss-bkw.html
+
+alloc 在栈上分配内存，自动释放
+malloc在堆上分配内存，需要手动释放，不会初始化
+calloc在堆上分配内存，和malloc类似，只是会自动初始化内存值为0
+realloc在堆上分配内存，用于扩充或缩小内存
