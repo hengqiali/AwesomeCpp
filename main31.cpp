aea13514f5fa51c03c2fd0b73b88ea7c33170150
chali <43441423+nwpuhq@users.noreply.github.com>
2019-08-24 18:19:26 +0800

    指针所占内存和类型的关系

--- main31.cpp
+++ main31.cpp
@@ -22,7 +22,9 @@ int main()
     std::cout << sizeof((int *)malloc(10)) << endl;
     std::cout << sizeof((double *)malloc(10)) << endl;
     std::cout << sizeof((char *)malloc(10)) << endl;
-
+    //答案都是4
+    //即指针的内存大小只有CPU的位数有关，32位的都是4个字节，与指针类型无关
+    //这个空间只是用来寻找和类型有关的内存
     return 0;
 
 }
