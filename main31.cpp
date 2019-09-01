6d9098edffdd72f6b113fe9404051286a5b90d82
chali <43441423+nwpuhq@users.noreply.github.com>
2019-09-01 23:08:05 +0800

    sizeof  ->  指针、string、char*、char[]

--- main31.cpp
+++ main31.cpp
@@ -11,6 +11,8 @@ using namespace std;
 
     C++ 关于指针变量所占内存空间大小的问题
     https://blog.csdn.net/cool_oyty/article/details/8078632
+    C++ string、char*、char[],sizeof不是函数，更像宏
+    https://blog.csdn.net/lanchunhui/article/details/50738498
 
 ***************************************/
 int main()
@@ -25,6 +27,30 @@ int main()
     //答案都是4
     //即指针的内存大小只有CPU的位数有关，32位的都是4个字节，与指针类型无关
     //这个空间只是用来寻找和类型有关的内存
+    
+    
+    string s = "";
+    string ss = "Hello";
+    char* css = "Hello";
+    char ccss[] = "Hello";
+    string sss = "Hlo";
+    cout << sizeof (string) << endl; //4
+    cout << sizeof (s) << endl;  //4
+    cout << sizeof (ss) << endl;  //4
+    cout << sizeof (css) << endl;  //4
+    cout << sizeof (ccss) << endl;  //6
+    cout << sizeof (sss) << endl;  //4
+    cout << sizeof (new string()) << endl; //4
+    //此时的sizeof里面传的其实是char *，'\0'也算一个字符
+    cout << sizeof ("Hello") << endl; //6
+    cout << sizeof ("He") << endl; //3
+    cout << sizeof ("H") << endl;  //2
+
+    int a = 0;
+
+    cout<<sizeof(a=3)<<endl;
+
+    cout<<a<<endl;
     return 0;
 
 }
